a40df566c2945e372c4c6e5f34e5729516887294
Nameless0l <wwwmbassiloic@gmail.com>
2022-08-08 15:16:58 +0100

    Loic

---
+++ lib/components/rounded_input.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/components/input_container.dart';
+import "package:loginpage/constantes.dart";
+
+
+class RoundedInput extends StatelessWidget {
+  const RoundedInput({
+    Key? key,
+    required this.icon,
+    required this.hint
+  }) : super(key: key);
+
+  final IconData icon;
+  final String hint;
+
+  @override
+  Widget build(BuildContext context) {
+    return InputContainer(
+      child: TextField(
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          icon: Icon(icon, color: kPrimaryColor),
+          hintText: hint,
+          border: InputBorder.none
+        ),
+      ));
+  }
+}
